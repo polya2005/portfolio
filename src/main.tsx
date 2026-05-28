93bfb7289793cb3eb696dc6ddb7b06827d49125d
polya2005 <thanpa28@mit.edu>
2026-05-28 16:42:54 +0700

    Add home route

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
+
 import App from "./App.tsx";
+import Home from "./Home.tsx";
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 );
