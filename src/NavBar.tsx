221a7d56a0b84449843e12e087fc2cbb749c1c10
polya2005 <thanpa28@mit.edu>
2026-05-29 18:07:03 +0700

    Change hover effect for NavLinks

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -8,22 +8,40 @@ function NavBar() {
         Than Thanpanit
       </NavLink>
       <div className="font-mono text-2xl flex space-x-8 text-text-primary">
-        <NavLink to="/" className="hover:text-accent">
+        <NavLink
+          to="/"
+          className="relative inline-block after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-accent after:origin-center after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+        >
           Home
         </NavLink>
-        <NavLink to="/projects" className="hover:text-accent">
+        <NavLink
+          to="/projects"
+          className="relative inline-block after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-accent after:origin-center after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+        >
           Projects
         </NavLink>
-        <NavLink to="/resume" className="hover:text-accent">
+        <NavLink
+          to="/resume"
+          className="relative inline-block after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-accent after:origin-center after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+        >
           Resume
         </NavLink>
-        <NavLink to="/about" className="hover:text-accent">
+        <NavLink
+          to="/about"
+          className="relative inline-block after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-accent after:origin-center after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+        >
           About
         </NavLink>
-        <NavLink to="/notes" className="hover:text-accent">
+        <NavLink
+          to="/notes"
+          className="relative inline-block after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-accent after:origin-center after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+        >
           Notes
         </NavLink>
-        <NavLink to="/contact" className="hover:text-accent">
+        <NavLink
+          to="/contact"
+          className="relative inline-block after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-accent after:origin-center after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+        >
           Contact
         </NavLink>
       </div>
