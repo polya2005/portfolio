61b593fd388112cd19178f9744457a84c5b0501f
polya2005 <thanpa28@mit.edu>
2026-06-20 11:35:50 +0700

    Include the label color in NoteTag

--- src/components/NoteTagLabel.tsx
+++ src/components/NoteTagLabel.tsx
@@ -7,23 +7,10 @@ type NoteTagLabelProps = {
 
 function NoteTagLabel({ tag }: NoteTagLabelProps) {
   return (
-    <span className={`px-1.5 py-1 text-xs font-mono rounded ${getTagColor(tag)}`}>
-      {tag}
+    <span className={`px-1.5 py-1 text-xs font-mono rounded ${tag.color}`}>
+      {tag.name}
     </span>
   );
-}
-
-function getTagColor(tag: NoteTag): string {
-  switch (tag) {
-    case NoteTag.Personal:
-      return "bg-green-200 text-green-800";
-    case NoteTag.Coursework:
-      return "bg-blue-200 text-blue-800";
-    case NoteTag.Project:
-      return "bg-yellow-200 text-yellow-800";
-    default:
-      return "bg-gray-200 text-gray-800";
-  }
 }
 
 export default NoteTagLabel;
--- src/types.ts
+++ src/types.ts
@@ -19,9 +19,9 @@ export interface TooltipContextType {
 
 // Note data types
 export const NoteTag = {
-  Personal: "personal",
-  Coursework: "coursework",
-  Project: "project",
+  Personal: { name: "personal", color: "bg-green-200 text-green-800" },
+  Coursework: { name: "coursework", color: "bg-blue-200 text-blue-800" },
+  Project: { name: "project", color: "bg-yellow-200 text-yellow-800" },
 } as const;
 
 export type NoteTag = (typeof NoteTag)[keyof typeof NoteTag];
