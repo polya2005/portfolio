134119c0ab76f101cda82ce09c6177c89f0eec69
polya2005 <thanpa28@mit.edu>
2026-06-20 10:49:13 +0700

    Include the label color in ProjectSkill and add new skills

--- src/components/ProjectSkillLabel.tsx
+++ src/components/ProjectSkillLabel.tsx
@@ -7,27 +7,10 @@ type ProjectSkillLabelProps = {
 
 function ProjectSkillLabel({ skill }: ProjectSkillLabelProps) {
   return (
-    <span
-      className={`px-1.5 py-1 text-xs font-mono rounded ${getSkillColor(
-        skill,
-      )}`}
-    >
-      {skill}
+    <span className={`px-1.5 py-1 text-xs font-mono rounded ${skill.color}`}>
+      {skill.name}
     </span>
   );
-}
-
-function getSkillColor(skill: ProjectSkill): string {
-  switch (skill) {
-    case ProjectSkill.Cpp:
-      return "bg-blue-200 text-blue-800";
-    case ProjectSkill.Python:
-      return "bg-yellow-200 text-blue-600";
-    case ProjectSkill.PcbDesign:
-      return "bg-green-200 text-green-800";
-    default:
-      return "bg-gray-200 text-gray-800";
-  }
 }
 
 export default ProjectSkillLabel;
--- src/types.ts
+++ src/types.ts
@@ -37,12 +37,16 @@ export type NoteItem = {
 
 // Project data types
 export const ProjectSkill = {
-  C: "C",
-  Cpp: "C++",
-  Python: "Python",
-  PcbDesign: "PCB Design",
-  LaserCutting: "Laser Cutting",
-  ThreeDPrinting: "3D Printing",
+  C: { name: "C", color: "bg-blue-200 text-blue-800" },
+  Cpp: { name: "C++", color: "bg-blue-200 text-blue-800" },
+  Python: { name: "Python", color: "bg-yellow-200 text-yellow-800" },
+  PcbDesign: { name: "PCB Design", color: "bg-green-200 text-green-800" },
+  LaserCutting: { name: "Laser Cutting", color: "bg-red-200 text-red-800" },
+  ThreeDPrinting: {
+    name: "3D Printing",
+    color: "bg-orange-200 text-orange-800",
+  },
+  Verilog: { name: "Verilog", color: "bg-zinc-200 text-zinc-800" },
 };
 
 export type ProjectSkill = (typeof ProjectSkill)[keyof typeof ProjectSkill];
