30154f26d4ecc10dd7d44e613219c438ea5fc3ca
polya2005 <thanpa28@mit.edu>
2026-07-03 14:00:33 +0700

    Add FPGA Spinthesizer project writeup

--- src/projects.ts
+++ src/projects.ts
@@ -2,18 +2,18 @@ import { ProjectSkill, type ProjectItem } from "./types";
 
 const projects: Map<string, ProjectItem> = new Map([
   [
-    "spaidv2",
+    "FPGA Spinthesizer",
     {
-      title: "SPAIDv2: Star Pointing and Identification Device",
-      description:
-        "I developed a second version of the SPAID device, which is more robust and has improved pointing algorithm compared to the first prototype. This is a work in progress.",
+      title: "FPGA Spinthesizer",
+      description: "In a team of two, we built the FPGA Spinthesizer, a MIDI-compatible, stepper-motor-based synthesizer which also improvises a musical response to the user's tunes. I was responsible for the high-level system design, tone generation on the stepper motors, PCB design, and mechanical assembly.",
       skills: [
-        ProjectSkill.Cpp,
+        ProjectSkill.Verilog,
+        ProjectSkill.PcbDesign,
         ProjectSkill.Python,
         ProjectSkill.ThreeDPrinting,
-        ProjectSkill.PcbDesign,
+        ProjectSkill.LaserCutting,
       ],
-      fileName: "spaidv2/spaidv2.md",
+      fileName: "fpga-spinthesizer/fpga-spinthesizer.md",
     },
   ],
   [
@@ -29,6 +29,21 @@ const projects: Map<string, ProjectItem> = new Map([
       ],
       fileName: "spaidv1/spaidv1.md",
       imagePath: "/projects/spaidv1/device.png",
+    },
+  ],
+  [
+    "spaidv2",
+    {
+      title: "SPAIDv2: Star Pointing and Identification Device",
+      description:
+        "I developed a second version of the SPAID device, which is more robust and has improved pointing algorithm compared to the first prototype. This is a work in progress.",
+      skills: [
+        ProjectSkill.Cpp,
+        ProjectSkill.Python,
+        ProjectSkill.ThreeDPrinting,
+        ProjectSkill.PcbDesign,
+      ],
+      fileName: "spaidv2/spaidv2.md",
     },
   ],
 ]);
