42d33b485c5375ffe1735c45eb3ff3e0fca460f1
polya2005 <thanpa28@mit.edu>
2026-07-23 22:50:24 -0400

    Use custom logo for both the favicon and the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,8 +4,12 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <div className="p-6 flex items-baseline justify-between bg-bg-elevated rounded-xl grow-0 shrink sticky top-0 z-50 shadow">
-      <NavLink to="/" className="font-mono text-2xl text-accent-bright">
-        Than Thanpanit
+      <NavLink to="/">
+        <img
+          src={`${import.meta.env.BASE_URL}logo.svg`}
+          alt="Logo"
+          className="h-10 w-10 inline-block mr-2"
+        />
       </NavLink>
       <div className="font-mono text-2xl flex space-x-8 text-text-primary">
         <NavLink
